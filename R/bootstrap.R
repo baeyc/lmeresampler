18240ba16fdbc9c0c102f4f77059a1144cdcdd99
Spenser Steele <spenser.steele@gmail.com>
2014-07-14 11:50:38 -0500

    inserted code for lev2 samps

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -173,7 +173,9 @@ residual.lmerMod <- function (model, fn, B){
 #'   @cite vanderLeeden:208kv
 case.lmerMod <- function (model, fn, B){
   # Draw sample of size J from level-2 units
+  level.two.samps <- simulate(object = model, nsim = B, replace = TRUE)
   # For each sample, draw a sample of the cases from the level-2 unit
+  # Use lapply to draw a sample of each case over level.two.samps
   # Plugin to .bootstrap.completion
   
   ## Or alt option
